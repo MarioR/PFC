1639f79c6ddbb9fa8189c8ff64806dbf2f69ff40
MarioR <mario.esc.rb@gmail.com>
2011-08-17 17:42:02 +0200

    Evento RATON.JS, no va como debería en 1 caso

---
+++ circulo.js
@@ -0,0 +1,9 @@
+/*
+ * 
+ * Esta es la clase de los círculos, en la cual se inicializarán, se actualizarán sus
+ * posiciones, se dibujarán sobre pantalla y guardarán la posición x e y y el color
+ * que tiene ese círculo.
+ * 
+ */
+ 
+
