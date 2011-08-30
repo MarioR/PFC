b7d390c89a6b926d28f198ef8b18aa143344295a
MarioR <mario.esc.rb@gmail.com>
2011-08-30 14:06:53 +0200

    Comenzando con el Estado de Juego, vamos por pasos

--- estadoPantJuego.js
+++ estadoPantJuego.js
@@ -1,1 +1,60 @@
 //FALTA DE PONER. ESTA TIENE LA MIGA DE TODO
+
+/*
+* Este fichero será el que contenga las funciones y variables necesarias para la pantalla de pausa del programa.
+*/
+
+var imagelist = [
+     { "src" : './imagenes/escenario1.jpg', "posx" : 0, "posy" : 0, "w" : 1000, "h" : 650},
+     { "src" : './imagenes/nami.jpg', "posx" : 0, "posy" : 0, "w" : 330, "h" : 321},
+     { "src" : './imagenes/zoro.jpg', "posx" : 300, "posy" : 300, "w" : 200, "h" : 200}     
+
+];
+
+//Pese a que está la misma imagen, no es repetir código, puesto que no está en la misma posicion
+//es otra imagen diferente
+var obstaculos = [
+	{"src" : './imagenes/wall.jpg', "posx" : 300, "posy" : 0, "w" : 50, "h" : 50, "num": 100},
+	{"src" : './imagenes/infernal.jpg', "posx" : 0, "posy" : 300, "w" : 50, "h" : 50, "num": 85},
+	{"src" : './imagenes/mine.jpg', "posx" : 200, "posy" : 550, "w" : 50, "h" : 50, "num": 30},
+	{"src" : './imagenes/wall.jpg', "posx" : 100, "posy" : 350, "w" : 50, "h" : 50, "num": 100},
+	{"src" : './imagenes/infernal.jpg', "posx" : 500, "posy" : 150, "w" : 50, "h" : 50, "num": 85},
+	{"src" : './imagenes/mine.jpg', "posx" : 800, "posy" : 500, "w" : 50, "h" : 50, "num": 30}
+	
+];
+
+
+//Esta función será la que pintará las imagenes de cada estado.
+function draw(){
+
+
+	
+}
+
+
+//Esta función se va autohaciendo a medida que avanzo en la creacion de la clase, ya que no se las variables que se utilizan
+function inicializarEstado(){
+
+
+	
+}
+	
+	
+//Esta función leerá el teclado y el ratón
+function leerAccion(){
+	
+
+
+	
+}
+
+
+//Esta función actualizará el juego
+
+
+function updateEstado(){
+	
+
+
+	
+}
